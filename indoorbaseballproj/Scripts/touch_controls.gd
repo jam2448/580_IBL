b686feb55e252c773f1ead71f61828220e7f6135
Jarin Moses <114194303+jam2448@users.noreply.github.com>
2025-09-10 09:33:31 -0400

    began working on controls

---
+++ indoorbaseballproj/Scripts/touch_controls.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var advance = $Advance
+@onready var returnRunners = $Return
+@onready var swing = $Swing
+
+func _on_advance_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_advance_released() -> void:
+	pass # Replace with function body.
