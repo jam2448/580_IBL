052ce829cd69d0fa69c39e921e4bb1cbae88ca55
Jarin Moses <114194303+jam2448@users.noreply.github.com>
2025-09-14 22:48:43 -0400

    Got touch controls linked to movement

--- indoorbaseballproj/Scripts/batter.gd
+++ indoorbaseballproj/Scripts/batter.gd
@@ -1,26 +1,53 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 30.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity");
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var advancing = false
+var returning = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if advancing:
+		velocity.x = SPEED
+	elif returning:
+		velocity.x = -SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
 	move_and_slide()
+	
+func move_right():
+	advancing = true
+	
+func stop_move_right():
+	advancing = false
+
+func move_left():
+	returning = true
+	
+func stop_move_left():
+	returning = false
+	
+#
+#func _physics_process(delta: float) -> void:
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
--- indoorbaseballproj/Scripts/touch_controls.gd
+++ indoorbaseballproj/Scripts/touch_controls.gd
@@ -1,12 +1,45 @@
 extends CanvasLayer
 
+@export var player_path: NodePath
+
+@onready var player: CharacterBody2D = get_node(player_path)
+
 @onready var advance = $Advance
 @onready var returnRunners = $Return
 @onready var swing = $Swing
 
+func _ready():
+	print("Found player:", player)  # should print the batter node
+
 func _on_advance_pressed() -> void:
-	pass # Replace with function body.
+	#advance.modulate.a = 0.5
+	player.move_right()
+	print("advance runners pressesd")
 
 
 func _on_advance_released() -> void:
-	pass # Replace with function body.
+	#advance.modulate.a = 1.0
+	player.stop_move_right()
+	print("advance runners released")
+
+
+func _on_return_pressed() -> void:
+	#returnRunners.modulate.a = 0.5
+	player.move_left()
+	print("return runners pressed")
+
+
+func _on_return_released() -> void:
+	#returnRunners.modulate = 1.0
+	player.stop_move_left()
+	print("return runners released")
+
+
+func _on_swing_pressed() -> void:
+	#swing.modulate.a = 0.5
+	print("swing pressed")
+
+
+func _on_swing_released() -> void:
+	#swing.modulate.a = 1.0
+	print("swing released")
